2afd35b1bf3dd727d138411d99d01903e7339803
trigger <triggerhappy187@live.com>
2023-11-10 01:06:03 -0800

    misc fixes/updates
    - camo can't go past 95%
    - don't adjust camo if stealth is equipped
    - add CamoIndexValue config value

--- MGS3CrouchWalk/dllmain.cpp
+++ MGS3CrouchWalk/dllmain.cpp
@@ -7,6 +7,7 @@ HMODULE GameModule = GetModuleHandleA("METAL GEAR SOLID3.exe");
 uintptr_t GameBase = (uintptr_t)GameModule;
 uintptr_t* CamoIndexData = NULL;
 float CamoIndexModifier = 1.0f;
+int CamoIndexValue = 0;
 mINI::INIStructure Config;
 
 typedef uintptr_t* __fastcall InitializeCamoIndexDelegate(uintptr_t a1, int a2);
@@ -24,11 +25,17 @@ uintptr_t __fastcall CalculateCamoIndexHook(uintptr_t a1, int a2)
         auto camoIndex = (int*)((char*)&CamoIndexData[4] + index + 4);
         auto movementState = (int*)((char*)&CamoIndexData[4] + index + 8);
 
+        if (*camoIndex >= 1000) // ignore if stealth is equipped
+        {
+            return result;
+        }
+
         if (*movementState == 0x6002)
         {
              *camoIndex = *camoIndex < 0 ? *camoIndex / CamoIndexModifier : *camoIndex * CamoIndexModifier;
+             *camoIndex += CamoIndexValue;
 
-            if (*camoIndex > 1000) *camoIndex = 1000;
+            if (*camoIndex > 950) *camoIndex = 950;
             if (*camoIndex < -1000) *camoIndex = -1000;
         }
     }
@@ -40,12 +47,12 @@ void InstallHooks()
 {
     int status = MH_Initialize();
 
-    uintptr_t CalcuateCamoIndexOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 83 EC 30 0F 29 74 24 20 48 8B F9 48 63 F2 E8") - 0x10;
-    uintptr_t InitializeCamoIndexOffset = (uintptr_t)Memory::PatternScan(GameModule, "85 D2 75 33 0F 57 C0 48 63 C2 48 C1 E0 07 48 8D");
+    uintptr_t calcuateCamoIndexOffset = (uintptr_t)Memory::PatternScan(GameModule, "48 83 EC 30 0F 29 74 24 20 48 8B F9 48 63 F2 E8") - 0x10;
+    uintptr_t initializeCamoIndexOffset = (uintptr_t)Memory::PatternScan(GameModule, "85 D2 75 33 0F 57 C0 48 63 C2 48 C1 E0 07 48 8D");
 
-    Memory::DetourFunction(CalcuateCamoIndexOffset, (LPVOID)CalculateCamoIndexHook, (LPVOID*)&CalculateCamoIndex);
+    Memory::DetourFunction(calcuateCamoIndexOffset, (LPVOID)CalculateCamoIndexHook, (LPVOID*)&CalculateCamoIndex);
 
-    InitializeCamoIndex = (InitializeCamoIndexDelegate*)InitializeCamoIndexOffset;
+    InitializeCamoIndex = (InitializeCamoIndexDelegate*)initializeCamoIndexOffset;
     CamoIndexData = InitializeCamoIndex(0, 0);
 }
 
@@ -54,6 +61,7 @@ void ReadConfig()
     mINI::INIFile file("MGS3CrouchWalk.ini");
     file.read(Config);
     CamoIndexModifier = std::stof(Config["Settings"]["CamoIndexModifier"]);
+    CamoIndexValue = std::stoi(Config["Settings"]["CamoIndexValue"]) * 10;
 }
 
 DWORD WINAPI MainThread(LPVOID lpParam)
